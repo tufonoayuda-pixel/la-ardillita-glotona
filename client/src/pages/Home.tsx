548f921bc52386fc86870a71e24e44721e6d1fe2
tufonoayuda-pixel <tufonoayuda@gmail.com>
2025-10-07 21:44:25 -0300

    Create Home.tsx

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import StoryBook from "@/components/StoryBook";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-background">
+      <StoryBook />
+    </div>
+  );
+}
